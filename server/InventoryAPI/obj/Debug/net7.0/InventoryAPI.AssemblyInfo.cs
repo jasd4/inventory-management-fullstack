82eff4a3d6e57162d3ff20bd606dc3d5b26bb908
jasd4 <jasdavis480@gmail.com>
2025-07-15 21:40:16 -0500

    Update project files and dependencies for InventoryAPI

--- server/InventoryAPI/obj/Debug/net7.0/InventoryAPI.AssemblyInfo.cs
+++ server/InventoryAPI/obj/Debug/net7.0/InventoryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af941ab90aa2bb74e96cab34b8dd94e1109c37a")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
